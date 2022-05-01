0ecadc94c40f2f692ee19143cadeef0bca1d6c8c
mayurrkukreja <mayurkukreja02@gmail.com>
2022-05-01 18:43:07 +0530

    inheritance

--- 20.inheritance-n-overriding.dart
+++ 20.inheritance-n-overriding.dart
@@ -1,17 +1,18 @@
-class Gopal {
+class Dad {
   void output1() {
     print("Hello Gaurav! Dad Here, How are you?");
   }
+
   void mom() {
     print("Khaane me kya banau?");
   }
 }
 
-class Gaurav extends Gopal {
+class Gaurav extends Dad {
   void outputGaurav() {
     print("Hello Dad! I'm Good How are you!");
   }
-  
+
   // Method Overriding...
   void mom() {
     print("Aaj mai bahar khauga!");
@@ -19,10 +20,9 @@ class Gaurav extends Gopal {
 }
 
 void main() {
-    var obj = Gaurav();
-    
-  
-    obj.mom();
-    obj.output1();
-    obj.outputGaurav();
+  var obj = Gaurav();
+
+  obj.mom();
+  obj.output1();
+  obj.outputGaurav();
 }
